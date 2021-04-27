c8da874d33538f1e0ffd2a6e1282550b52736dcc
Nguyễn Quang Huy <quanghuy1998kh@gmail.com>
2021-04-27 09:04:07 +0700

    u

--- lib/config.dart
+++ lib/config.dart
@@ -1,13 +1,13 @@
 class Config {
   // debug url
-  static String httpUri = 'https://vnrealtor.herokuapp.com/graphql';
-  static String wsUri = 'wss://vnrealtor.herokuapp.com/graphql';
+  // static String httpUri = 'https://vnrealtor.herokuapp.com/graphql';
+  // static String wsUri = 'wss://vnrealtor.herokuapp.com/graphql';
 
   // tester url
   // static String httpUri = 'https://vnrealtor-sq73uv5o7a-as.a.run.app/graphql';
   // static String wsUri = 'wss://vnrealtor-sq73uv5o7a-as.a.run.app/graphql';
 
   // production url
-  // static String httpUri = 'https://datcao-be-hv2wn47voq-as.a.run.app/graphql';
-  // static String wsUri = 'wss://datcao-be-hv2wn47voq-as.a.run.app/graphql';
+  static String httpUri = 'https://datcao-be-hv2wn47voq-as.a.run.app/graphql';
+  static String wsUri = 'wss://datcao-be-hv2wn47voq-as.a.run.app/graphql';
 }
