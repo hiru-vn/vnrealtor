6f57d394170da33db41f5de15cb851c38e1092f8
Nguyễn Quang Huy <quanghuy1998kh@gmail.com>
2021-07-20 10:11:13 +0700

    u

--- lib/modules/post/post_page.dart
+++ lib/modules/post/post_page.dart
@@ -282,7 +282,7 @@ Future<bool> _showRecomendDialog(BuildContext context) async {
       await SPref.instance.getBool('notShowRecomendAgain');
   if (notShowRecomendAgain) return Future.value(false);
   if (AuthBloc.firstLogin) return Future.value(false);
-  // if (AuthBloc.instance.userModel.totalPost == 0) return Future.value(false);
+  // if (AuthBloc.instance.userModel. == 0) return Future.value(false);
   if (AuthBloc.instance.userModel.role != 'EDITOR') return Future.value(false);
 
   return showDialog<bool>(
